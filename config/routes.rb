b9d34c36d09a04839004affe2c25281c46adf5bf
EC2 Default User <ec2-user@ip-172-31-38-211.ec2.internal>
2022-05-14 18:05:42 +0000

    [Upd] Changed from the homes controller to the home controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
    }
   
-  root to: 'homes#top'
+  root to: 'home#top'
   
   resources :users,only: [:index, :show, :edit]
   resources :books,only: [:new, :index, :show, :create, :edit]
   
-  get 'homes/about' => 'homes#about' ,as: 'about'
+  get 'home/about' => 'home#about' ,as: 'about'
   patch 'users/:id' => 'users#update', as: 'update_user'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
