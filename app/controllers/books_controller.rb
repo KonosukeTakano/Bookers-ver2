71cafcc0c75a1d74d0d48e4b8b8cff9700891200
EC2 Default User <ec2-user@ip-172-31-38-211.ec2.internal>
2022-05-20 10:58:29 +0000

    Margin over commented user image

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = User.find_by(id: @book.user_id) #books/showページで表示するユーザーのid
+    @book_comment = BookComment.new
   end
   
   def edit
