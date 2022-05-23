519f7c6ebb6036779fc4375b2e937bd955330b47
EC2 Default User <ec2-user@ip-172-31-38-211.ec2.internal>
2022-05-23 09:55:43 +0000

    [Upd] book_comment,validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,13 +10,14 @@ class BooksController < ApplicationController
     @user = current_user
     @users = User.all
   end
-  
+
   def show
     @book = Book.find(params[:id])
     @user = User.find_by(id: @book.user_id) #books/showページで表示するユーザーのid
     @book_comment = BookComment.new
+    #追記
   end
-  
+
   def edit
     @book = Book.find(params[:id])
     @books = Book.all
@@ -24,11 +25,11 @@ class BooksController < ApplicationController
       redirect_to books_path
     end
   end
-  
+
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    
+
     if @book.save
     #詳細画面へリダイレクト
       flash[:notice] = "You have updated book successfully."
@@ -37,9 +38,9 @@ class BooksController < ApplicationController
       render :edit
     end
   end
-  
+
   def create
-    
+
     @books = Book.all
     #データを受け取り新規登録するためのインスタンス作成
     @book = Book.new(book_params)
@@ -54,20 +55,20 @@ class BooksController < ApplicationController
       render :index
     end
   end
-  
+
   def destroy
     @book = Book.find(params[:id])  # データ（レコード）を1件取得
-    
+
     if @book.destroy  # データ（レコード）を削除
       flash[:notice] = "Book was successfully destroyed."
       redirect_to books_path  # 投稿一覧画面へリダイレクト
     end
   end
-  
+
   private
   #ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
