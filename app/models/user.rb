a1d164dfdcd350732b0d58b91fa67df6c1203aac
EC2 Default User <ec2-user@ip-172-31-38-211.ec2.internal>
2022-06-03 11:07:03 +0000

    [Add] relationship.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,11 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :relationships, source: :followed
+  has_many :followers, through: :relationships, source: :follower
+  
   validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
   
