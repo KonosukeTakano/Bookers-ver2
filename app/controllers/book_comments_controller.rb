e68e4468b2c02d19ee7c3f2d0e04e9e7be9ac1ec
EC2 Default User <ec2-user@ip-172-31-38-211.ec2.internal>
2022-06-07 04:08:42 +0000

    [Upd] Book_Comment, Ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,7 +4,6 @@ class BookCommentsController < ApplicationController
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
     if @book_comment.save
-      redirect_to book_path(@book)
     else
       @user = User.find_by(id: @book.user_id) #books/showページで表示するユーザーのid
       render template: "books/show"
@@ -12,16 +11,14 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
   end
-  
 
   private
 
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-  
 
 end
