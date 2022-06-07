68a7ec684e4bc53c5c7d78aaf6f48142a71a0dff
EC2 Default User <ec2-user@ip-172-31-38-211.ec2.internal>
2022-06-07 09:01:26 +0000

    [Upd] book_comment, ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,15 +4,19 @@ class BookCommentsController < ApplicationController
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
     if @book_comment.save
+      flash.now[:notice] = 'コメントを投稿しました'
+      render 'book_comments/comments'  #render先にjsファイルを指定
     else
       @user = User.find_by(id: @book.user_id) #books/showページで表示するユーザーのid
       render template: "books/show"
     end
   end
-
+  
   def destroy
     @book = Book.find(params[:book_id])
     BookComment.find(params[:id]).destroy
+    flash.now[:alert] = '投稿を削除しました'
+    render 'book_comments/comments'  #render先にjsファイルを指定
   end
 
   private
