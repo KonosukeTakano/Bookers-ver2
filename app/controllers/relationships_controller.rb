65a54bed367ddf47bb830ab8255c5f311a941472
EC2 Default User <ec2-user@ip-172-31-38-211.ec2.internal>
2022-06-08 07:24:49 +0000

    [Upd] UserInfo, Relationship, Ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,10 +1,12 @@
 class RelationshipsController < ApplicationController
-    
+  
+  before_action :set_user
+  
   def create
      following = current_user.follow(params[:user_id])
      if following.save
        flash[:success] = 'ユーザーをフォローしました'
-       redirect_back(fallback_location: root_path)
+       render 'users/follow'
      else
        flash.now[:alert] = 'ユーザーのフォローに失敗しました'
        redirect_back(fallback_location: root_path)
@@ -15,22 +17,27 @@ class RelationshipsController < ApplicationController
      following = current_user.unfollow(params[:user_id])
      if following.destroy
        flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_back(fallback_location: root_path)
+      render 'users/follow'
      else
        flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
       redirect_back(fallback_location: root_path)
      end
   end
-  
+
   # フォロー一覧
   def followings
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
     @users = user.followings
   end
   # フォロワー一覧
   def followers
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
     @users = user.followers
+  end
+  
+  private
+  def set_user
+    @user = User.find(params[:user_id])
   end
   
 end
