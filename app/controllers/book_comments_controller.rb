bc67afd97834b734a87ffed1ecd74b72ee7e15ca
EC2 Default User <ec2-user@ip-172-31-38-211.ec2.internal>
2022-06-09 06:13:16 +0000

    [Upd] flash message

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,12 +4,12 @@ class BookCommentsController < ApplicationController
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
     if @book_comment.save
-      flash[:notice] = 'コメントを投稿しました'
+      flash.now[:notice] = 'コメントを投稿しました'
       render 'book_comments/comments'  #render先にjsファイルを指定
     else
-      flash[:notice] = 'コメント投稿に失敗'
       @user = User.find_by(id: @book.user_id) #books/showページで表示するユーザーのid
-      render template: "books/show"
+      # render template: "books/show"
+      render :error #render先にjsファイルを指定
     end
   end
   
