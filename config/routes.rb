6d25f31b663af04214b3fcac09e0f3a97c07fa00
EC2 Default User <ec2-user@ip-172-31-38-211.ec2.internal>
2022-06-09 16:01:18 +0000

    [Upd] Guest Log in

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
    }
-
+  
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+  
   root to: 'home#top'
 
   resources :users,only: [:index, :show, :edit]
